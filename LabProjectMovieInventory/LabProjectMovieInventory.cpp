1f0c3be6d2f8ccae08588f853b5cf79541c5a4be
IonTank5 <61195075+IonTank5@users.noreply.github.com>
2020-10-26 17:04:00 -0400

    Updated to fit new requirements

--- LabProjectMovieInventory/LabProjectMovieInventory.cpp
+++ LabProjectMovieInventory/LabProjectMovieInventory.cpp
@@ -6,19 +6,20 @@ Extra: Getting user input for the names of movies
 */
 #include <iostream>
 #include <iomanip>
+#include <string>
 #include "movieInfo.h"
 using std::cout; using std::cin; using std::endl; using std::string;
-void printOut(movieInfo video1, movieInfo video2, movieInfo video3) {
-    cout << std::setw(35) << std::left << "Video Title" //Putting a line after printing each variable to make it easier to read
+void printOut(movieInfo video1, movieInfo video2, movieInfo video3, int length) {
+    cout << std::setw(length) << std::left << "Video Title" //Putting a line after printing each variable to make it easier to read
          << std::setw(10) << std::left << "Cost" 
          << std::setw(15) << std::left << " # renting" << endl;
-    cout << std::setw(35) << std::left << video1.getName() << "$"
+    cout << std::setw(length) << std::left << video1.getName() << "$"
          << std::fixed << std::showpoint << std::setw(10) << std::left << std::setprecision(2) << video1.getCost() 
          << std::setw(15) << std::left << std::noshowpoint << std::setprecision(0) << video1.getRent() << endl;
-    cout << std::setw(35) << std::left << video2.getName() << "$"
+    cout << std::setw(length) << std::left << video2.getName() << "$"
          << std::fixed << std::showpoint << std::setw(10) << std::left << std::setprecision(2) << video2.getCost()
          << std::setw(15) << std::left << std::noshowpoint << std::setprecision(0) << video2.getRent() << endl;
-    cout << std::setw(35) << std::left << video3.getName() << "$"
+    cout << std::setw(length) << std::left << video3.getName() << "$"
          << std::fixed << std::showpoint << std::setw(10) << std::left << std::setprecision(2) << video3.getCost()
          << std::setw(15) << std::left << std::noshowpoint << std::setprecision(0) << video3.getRent() << endl;
 }
@@ -34,6 +35,27 @@ double printTotal(movieInfo a, movieInfo b, movieInfo c) {
     return total;
 }
 
+int longestMovie(movieInfo a, movieInfo b, movieInfo c) {
+    int length = 0, temp;
+    if (a.getName().length() > b.getName().length()) {
+        if (a.getName().length() > c.getName().length()) {
+            length = a.getName().length();
+        }
+        else {
+            length = c.getName().length();
+        }
+    }
+    else {
+        if (b.getName().length() > c.getName().length()) {
+            length = b.getName().length();
+        }
+        else {
+            length = c.getName().length();
+        }
+    }
+    return length;
+}
+
 int main()
 {
     double tempCost;
@@ -41,13 +63,13 @@ int main()
     string temp;
     //Extra: Getting user input for the titles of the movies
     cout << "Enter title for 1st Video: ";
-    cin >> temp;
+    std::getline (cin, temp);
     movieInfo video1(temp); //Creating object with the input
     cout << "Enter title for 2nd Video: ";
-    cin >> temp;
+    std::getline(cin, temp);
     movieInfo video2(temp);
     cout << "Enter title for 3rd Video: ";
-    cin >> temp;
+    std::getline(cin, temp);
     movieInfo video3(temp);
     //Getting the cost and rent info for each video
     video1.setCost(1);
@@ -57,7 +79,8 @@ int main()
     video3.setCost(3);
     video3.setRent(3);    
     //Printing out the info;
-    printOut(video1, video2, video3);
+    int length = longestMovie(video1, video2, video3);
+    printOut(video1, video2, video3, length+5);
     cout << endl << endl;
     double total = printTotal(video1, video2, video3);
     printAverage(video1, video2, video3, total);
--- LabProjectMovieInventory/movieInfo.cpp
+++ LabProjectMovieInventory/movieInfo.cpp
@@ -11,24 +11,30 @@ void movieInfo::setCost(int x) {
                 break;
             }
             else {
-                std::cout << "Invalid input";
+                std::cout << "Please enter non negative number" << std::endl;
             }
         }
         cost = temp;
 }
 void movieInfo::setRent(int x) {
-    int temp;
+    double temp;
     while (true) {
         std::cout << "How many are renting " << name << " ";
         std::cin >> temp;
-        if (temp >= 0) {
+        if (temp >= 0 && temp==floor(temp)) {
             break;
         }
+        else if (temp < 0 && temp!=floor(temp)) {
+            std::cout << "Please enter non negative whole number " << std::endl;
+        }
+        else if(!temp >= 0){
+            std::cout << "Please enter non negative number" << std::endl;
+        }
         else {
-            std::cout << "Invalid input";
+            std::cout << "Please enter whole number" << std::endl;
         }
     }
-    rent = temp;
+    rent = int(temp);
 }
 
 std::string movieInfo::getName() {
